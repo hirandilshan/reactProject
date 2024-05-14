23d9e5a4289310aa35b9e9228663f77efef9ebd4
Hiran Nishshanka <116668378+hirandilshan@users.noreply.github.com>
2024-05-14 23:58:28 +0530

    remove logout

--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -70,7 +70,7 @@ router.route("/token").get(verifyUser, (req, res) => {
 });
 
 router.route("/logout").get((req, res) => {
-  res.clearCookie("token", { httpOnly: true, secure: true, sameSite: 'None' });
+  res.clearCookie("token");
   res.status(200).json({ message: "logout" });
 });
 
